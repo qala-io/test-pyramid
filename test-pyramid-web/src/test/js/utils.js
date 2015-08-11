e16f07e71d57b8b6623112dfdc55d43fc4c45a9f
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@qala.io>
2015-08-11 22:35:37 +0300

    Fix test bug with random number

--- test-pyramid-web/src/test/js/utils.js
+++ test-pyramid-web/src/test/js/utils.js
@@ -3,7 +3,8 @@ var random = require('random-strings').random;
 var moreThanZero = function() {
   var alphabet = '123456789';
   var nOfDigits = random(1, '123');
-  return +random(nOfDigits, alphabet);
+  var stringNumber = random(+nOfDigits, alphabet);
+  return +stringNumber;
 };
 
 exports.moreThanZero = moreThanZero;
